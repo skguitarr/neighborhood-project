c9e3231a391bc7cc4b80ff6482fc3b6350e103d9
vagrant <vagrant@vagrant.vm>
2018-05-01 21:54:00 +0000

    Fixed styling and set up navigation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 var ViewModel = function () {
-  this.places = ko.observableArray(
+  var self = this;
+
+  self.places = ko.observableArray(
     [
-      { address: '3199 Maple Dr NE, Atlanta, GA 30305' },
-      { address: '3393 Peachtree Rd NE' },
-      { address: '3050 Peachtree Rd NW, Atlanta, GA 30305' },
-      { address: 'Caribou Coffee, Peachtree Road Northeast, Atlanta, GA' },
-      { address: '3101 Piedmont Rd NE, Atlanta, GA' }
+      { address: '3199 Maple Dr NE, Atlanta, GA 30305', extraClass: 'active' },
+      { address: '3393 Peachtree Rd NE', extraClass: '' },
+      { address: '3050 Peachtree Rd NW, Atlanta, GA 30305', extraClass: '' },
+      { address: 'Caribou Coffee, Peachtree Road Northeast, Atlanta, GA', extraClass: '' },
+      { address: '3101 Piedmont Rd NE, Atlanta, GA', extraClass: '' }
     ]
   );
 }
